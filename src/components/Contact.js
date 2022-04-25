816d130d2e82dc45c5854d59284ec1b72523fed5
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-04-25 13:04:36 +0200

    added img,buttons and some text to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import profileImage from '../images/profileimage.jpg'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import{faLinkedin} from '@fortawesome/free-brands-svg-icons'
 export default function Contact(){
 
+  
     return(
-        <h1>contact works</h1>
+      <div className='contacts-container'>
+        <img src={profileImage} className='profile-image' alt="profile potret"></img>
+            <header className='header-container'>
+                <h3>David O Ciarnain</h3>
+                <p>Java developer student</p>
+                <div className='header-button'>
+                    <button><FontAwesomeIcon icon ={faEnvelope}></FontAwesomeIcon> Email </button>
+                    <button> <FontAwesomeIcon icon = {faLinkedin}> </FontAwesomeIcon>linkedin</button>
+                </div>
+            </header>
+        </div>
     )
 }
